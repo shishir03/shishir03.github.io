0bc3444520bca0ff34771e5a36ffa112b61025ce
Shishir Iyer <shiyer@ucsd.edu>
2023-09-08 15:58:19 -0700

    added basic project listing

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", async function() {
     const home = document.getElementById("home-but");
     const about = document.getElementById("about-but");
     const resume = document.getElementById("resume-but");
@@ -24,4 +24,18 @@ window.addEventListener("load", function() {
     socials.addEventListener('click', function() {
         window.location.href = "index.html#socials";
     });
+
+    const response = await fetch("https://api.github.com/users/shishir03/repos");
+    let repos = await response.json();
+    repos = repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+    console.log(repos);
+    
+    for(let i = 0; i < repos.length; i++) {
+        let r = repos[i];
+        console.log(r);
+        const projectDiv = document.getElementById("projects");
+        projectDiv.innerHTML += `
+        <h2 class="repo-name"><a href="${r.svn_url}">${r.name}</a></h2>
+        `;
+    }
 });
